4f5b713c38e0524476a41e77b8cc19fdabbcb051
kyriri <git-kyriri@opayq.com>
2021-04-22 12:51:39 -0300

    Connect to local DB "fyyur"

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -12,7 +12,4 @@ DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Connect to the database
-
-
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql:///fyyur'
