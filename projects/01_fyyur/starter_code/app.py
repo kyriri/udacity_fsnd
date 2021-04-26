bce66a60e8e39ea7ddd2ef61918071bebb151aba
kyriri <git-kyriri@opayq.com>
2021-04-26 18:32:40 -0300

    Migrate db, create 'artists' table

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -53,7 +53,7 @@ class Artist(db.Model):
   __tablename__ = 'artists'
 
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
+  name = db.Column(db.String, nullable=False)
   city = db.Column(db.String(120))
   state = db.Column(db.String(120))
   phone = db.Column(db.String(120))
